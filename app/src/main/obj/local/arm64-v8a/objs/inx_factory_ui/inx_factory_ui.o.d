d5725f9f0e825175109a3820128c94c60fca02a3
Joe <d1004311049@gmail.com>
2022-05-15 23:08:25 +0800

    delete not need files

--- app/src/main/obj/local/arm64-v8a/objs/inx_factory_ui/inx_factory_ui.o.d
+++
@@ -1,2 +0,0 @@
-C:/Users/joe.zy.chen/Desktop/android/android_factory_ui/app/src/main/obj/local/arm64-v8a/objs/inx_factory_ui/inx_factory_ui.o: \
-  C:\Users\joe.zy.chen\Desktop\android\android_factory_ui\app\src\main\jni\inx_factory_ui.c
--- app/src/main/obj/local/x86/objs/inx_factory_ui/inx_factory_ui.o.d
+++
@@ -1,2 +0,0 @@
-C:/Users/joe.zy.chen/Desktop/android/android_factory_ui/app/src/main/obj/local/x86/objs/inx_factory_ui/inx_factory_ui.o: \
-  C:\Users\joe.zy.chen\Desktop\android\android_factory_ui\app\src\main\jni\inx_factory_ui.c
